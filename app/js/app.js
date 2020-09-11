7e6b41546d7293cbc0588f1c27f069af90c6eaab
Yevheniia Spaska <yevheniiaspaska@MacBook-Pro-Yevheniia.local>
2020-09-11 19:04:07 +0300

    develop Init commit

---
+++ app/js/app.js
@@ -0,0 +1,17 @@
+$(document).on("click", ".main-menu a", function() {
+  $(this)
+    .addClass("active")
+    .siblings()
+    .removeClass("active");
+});
+
+function slowScroll(id) {
+  var offset = 88;
+  $("html, body").animate(
+    {
+      scrollTop: $(id).offset().top - offset
+    },
+    500
+  );
+  return false;
+}
